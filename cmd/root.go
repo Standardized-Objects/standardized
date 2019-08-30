accc2f14bf5428f159da8c2a7a7c28b4ac0d31c1
Juan Ezquerro <juan@syncrtc.com>
2019-08-30 23:47:44 +0200

    setup author

--- cmd/root.go
+++ cmd/root.go
@@ -55,17 +55,7 @@ func Execute() {
 
 func init() {
   cobra.OnInitialize(initConfig)
-
-  // Here you will define your flags and configuration settings.
-  // Cobra supports persistent flags, which, if defined here,
-  // will be global for your application.
-
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.standardized.yaml)")
-
-
-  // Cobra also supports local flags, which will only run
-  // when this action is called directly.
-  rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.standardized/config.yaml)")
 }
 
 
@@ -83,8 +73,8 @@ func initConfig() {
     }
 
     // Search config in home directory with name ".standardized" (without extension).
-    viper.AddConfigPath(home)
-    viper.SetConfigName(".standardized")
+    viper.AddConfigPath(home + "/.standardized")
+    viper.SetConfigName("config.yaml")
   }
 
   viper.AutomaticEnv() // read in environment variables that match
