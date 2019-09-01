a8f10b534db8b60b036f1f48908020fc3a3f06c0
Juan Ezquerro <juan@syncrtc.com>
2019-09-01 14:12:14 +0200

    config

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+Copyright © 2019 Juan Ezquerro LLanes <arrase@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,14 +20,7 @@ import (
   "fmt"
   "os"
   "github.com/spf13/cobra"
-  "path/filepath"
-  homedir "github.com/mitchellh/go-homedir"
-  "github.com/spf13/viper"
-
 )
-
-
-var cfgFile string
 
 
 // rootCmd represents the base command when called without any subcommands
@@ -42,44 +35,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
   if err := rootCmd.Execute(); err != nil {
     fmt.Println(err)
     os.Exit(1)
   }
 }
-
-func init() {
-  cobra.OnInitialize(initConfig)
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is " + filepath.Join("$HOME",".standardized","config.yaml") + ")")
-}
-
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-  if cfgFile != "" {
-    // Use config file from the flag.
-    viper.SetConfigFile(cfgFile)
-  } else {
-    // Find home directory.
-    home, err := homedir.Dir()
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
-
-    // Search config in home directory with name ".standardized" (without extension).
-    viper.AddConfigPath(filepath.Join(home , ".standardized"))
-    viper.SetConfigName("config")
-  }
-
-  viper.AutomaticEnv() // read in environment variables that match
-
-  // If a config file is found, read it in.
-  if err := viper.ReadInConfig(); err == nil {
-    fmt.Println("Using config file:", viper.ConfigFileUsed())
-  }
-}
-
