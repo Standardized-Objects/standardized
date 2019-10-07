e41a85fecbb9f480d330ff74a27af946f57bcdf8
Juan Ezquerro <juan@syncrtc.com>
2019-10-07 17:46:50 +0200

    Refactor Code

--- cmd/object_create.go
+++ cmd/object_create.go
@@ -18,14 +18,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"log"
 	"os"
-	"path/filepath"
 	"standardized/internal"
-	"strings"
 )
 
 var outputPath string
@@ -39,99 +34,22 @@ var createCmd = &cobra.Command{
 			os.Exit(0)
 		}
 
-		// Split object name into repo and folder
-		obj := strings.Split(args[0], "/")
-		if len(obj) != 2 {
-			fmt.Println("Invalid Object Definition : " + args[0])
-			os.Exit(0)
+		// Load object definition
+		o := tools.ObjDef{
+			Name:   args[0],
+			OutDir: outputPath,
 		}
-
-		// Copy templates from object definition
-		curr_dir, _ := os.Getwd()
-
-		var obj_dir string
-		if obj[0] == "_local" {
-			obj_dir = filepath.Join(filepath.Join(curr_dir, ".stdized"), obj[1])
-		} else {
-			obj_dir = filepath.Join(filepath.Join(filepath.Join(tools.GetConfigDir(), obj[0]), "src"), obj[1])
-		}
-
-		if !tools.Exists(obj_dir) {
-			fmt.Println("Invalid path: " + obj_dir)
-			os.Exit(0)
-		}
-
-		templates_dir := filepath.Join(obj_dir, "templates")
-
-		var _out string
-		if outputPath != "" {
-			_out = filepath.Join(curr_dir, outputPath)
-		} else {
-			_out = curr_dir
-		}
-
-		tools.CreateIfNotExists(_out, os.ModePerm)
-
+		o.Load()
 		// Run pre create hooks
-		tools.RunHooks(filepath.Join(obj_dir, "precreate"), _out)
-		tools.CopyDirectory(templates_dir, _out)
-
-		// Read object configuration
-		viper.SetConfigType("yaml")
-		viper.SetConfigName("config")
-		viper.AddConfigPath(obj_dir)
-		err := viper.ReadInConfig()
-		if err != nil {
-			panic(fmt.Errorf("Fatal error config file: %s \n", err))
-		}
-
+		o.RunHooks("precreate")
 		// Apply values
-		values := viper.Get("values")
-		config := make(map[string]string, len(values.([]interface{})))
-
-		for _, data := range values.([]interface{}) {
-
-			prompt := promptui.Prompt{
-				Label: data.(map[interface{}]interface{})["description"].(string),
-			}
-
-			if _default, ok := data.(map[interface{}]interface{})["default"].(string); ok {
-				prompt.Default = _default
-			}
-
-			result, err := prompt.Run()
-
-			if err != nil {
-				fmt.Printf("Fail %v\n", err)
-				return
-			}
-
-			config[data.(map[interface{}]interface{})["tag"].(string)] = result
-		}
-
-		wlk_err := filepath.Walk(_out,
-			func(path string, info os.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
-
-				if !info.IsDir() {
-					tools.ParseTemplate(path, config)
-				}
-				return nil
-			})
-
-		if wlk_err != nil {
-			log.Println(wlk_err)
-			os.Exit(0)
-		}
-
+		o.Apply()
 		// Run post create hooks
-		tools.RunHooks(filepath.Join(obj_dir, "postreate"), _out)
+		o.RunHooks("postcreate")
 	},
 }
 
 func init() {
 	objectCmd.AddCommand(createCmd)
-	createCmd.Flags().StringVarP(&outputPath, "output", "o", "", "Output path, will be created if not exists")
+	createCmd.Flags().StringVarP(&outputPath, "output", "o", "_current_dir_", "Output path, will be created if not exists")
 }
---
+++ internal/objectTools.go
@@ -0,0 +1,136 @@
+/*
+Copyright © 2019 Juan Ezquerro LLanes <arrase@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package tools
+
+import (
+	"fmt"
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+type ObjDef struct {
+	Name    string
+	OutDir  string
+	Path    string
+	OutPath string
+}
+
+func (o *ObjDef) Load() {
+	// Split object name into repo and folder
+	obj := strings.Split(o.Name, "/")
+	if len(obj) != 2 {
+		fmt.Println("Invalid Object Definition : " + o.Name)
+		os.Exit(0)
+	}
+
+	curr_dir, _ := os.Getwd()
+	if obj[0] == "_local" {
+		o.Path = filepath.Join(filepath.Join(curr_dir, ".stdized"), obj[1])
+	} else {
+		o.Path = filepath.Join(filepath.Join(filepath.Join(GetConfigDir(), obj[0]), "src"), obj[1])
+	}
+
+	if o.OutDir != "_current_dir_" {
+		o.OutPath = filepath.Join(curr_dir, o.OutDir)
+		CreateIfNotExists(o.OutPath, os.ModePerm)
+	} else {
+		o.OutPath = curr_dir
+	}
+
+	if _, err := os.Stat(o.Path); os.IsNotExist(err) {
+		fmt.Println("Invalid path: " + o.Path)
+		os.Exit(0)
+	}
+}
+
+func (o *ObjDef) Apply() {
+	templates_dir := filepath.Join(o.Path, "templates")
+	CopyDirectory(templates_dir, o.OutPath)
+
+	// Read object configuration
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(o.Path)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	// Apply values
+	values := viper.Get("values")
+	config := make(map[string]string, len(values.([]interface{})))
+
+	for _, data := range values.([]interface{}) {
+
+		prompt := promptui.Prompt{
+			Label: data.(map[interface{}]interface{})["description"].(string),
+		}
+
+		if _default, ok := data.(map[interface{}]interface{})["default"].(string); ok {
+			prompt.Default = _default
+		}
+
+		result, err := prompt.Run()
+
+		if err != nil {
+			fmt.Printf("Fail %v\n", err)
+			return
+		}
+
+		config[data.(map[interface{}]interface{})["tag"].(string)] = result
+	}
+
+	wlk_err := filepath.Walk(o.OutPath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !info.IsDir() {
+				ParseTemplate(path, config)
+			}
+			return nil
+		})
+
+	if wlk_err != nil {
+		log.Println(wlk_err)
+		os.Exit(0)
+	}
+}
+
+func (o *ObjDef) RunHooks(hooks_dir string) {
+	scripts_path := filepath.Join(o.Path, hooks_dir)
+	if Exists(scripts_path) {
+		scripts, _ := ioutil.ReadDir(scripts_path)
+		for _, scpt := range scripts {
+			mode := scpt.Mode()
+			if !mode.IsDir() && scpt.Name()[:1] != "." {
+				log.Printf("Running hook: " + scpt.Name())
+				cmd := exec.Command(filepath.Join(scripts_path, scpt.Name()))
+				cmd.Dir = o.OutPath
+				cmd.Run()
+				log.Printf("Done: " + scpt.Name())
+			}
+		}
+	}
+}
